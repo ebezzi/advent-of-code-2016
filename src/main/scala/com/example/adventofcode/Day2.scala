813082ab40b2988f07e9db66d3f2377a5b8d1f90
Emanuele Bezzi <emanuele.bezzi@databiz.it>
2016-12-03 16:46:19 +0100

    day2 part2

--- src/main/scala/com/example/adventofcode/Day2.scala
+++ src/main/scala/com/example/adventofcode/Day2.scala
@@ -4,16 +4,12 @@ import com.example.adventofcode.Day1.Position
 
 object Day2 extends App {
 
-//  println (Inputs.day2.lines.toSeq.length)
-
   case class Point(x: Int, y: Int) {
     def left = if (x > 0) Point(x-1, y) else this
     def right = if (x < 2) Point(x+1, y) else this
     def up = if (y > 0) Point(x, y-1) else this
     def down = if (y < 2) Point(x, y+1) else this
   }
-
-//  def computeLine(line: Seq[Char])
 
   val res = Inputs.day2.lines.toList.scanLeft(Point(1,1)) { (fst, snd) =>
     snd.foldLeft(fst) { (prev, curr) =>
@@ -27,7 +23,5 @@ object Day2 extends App {
   }
 
   println(res.tail)
-
-
 
 }
