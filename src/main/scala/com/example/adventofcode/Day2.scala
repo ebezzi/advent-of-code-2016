12bf21d7195c18a7500d84b79fa5146517e76d9f
Emanuele Bezzi <emanuele.bezzi@databiz.it>
2016-12-03 16:32:02 +0100

    day2 part1

--- src/main/scala/com/example/adventofcode/Day2.scala
+++ src/main/scala/com/example/adventofcode/Day2.scala
@@ -4,35 +4,29 @@ import com.example.adventofcode.Day1.Position
 
 object Day2 extends App {
 
-  case class Position(x: Int, y: Int, facing: Char)
-  case class Accumulation(current: Position, history: Seq[Position])
+//  println (Inputs.day2.lines.toSeq.length)
 
-  // Part 1
-  val positions = Inputs.day1.split(", ")
-    .map(i => i.head -> i.tail.toInt)
-    .scanLeft(Position(0, 0, 'N')) { (pos, tup) =>
-      tup match {
-        case ('L', n) if pos.facing == 'N' =>
-          Position(pos.x - n, pos.y, 'W')
-        case ('L', n) if pos.facing == 'W' => Position(pos.x, pos.y - n, 'S')
-        case ('L', n) if pos.facing == 'E' => Position(pos.x, pos.y + n, 'N')
-        case ('L', n) if pos.facing == 'S' => Position(pos.x + n, pos.y, 'E')
+  case class Point(x: Int, y: Int) {
+    def left = if (x > 0) Point(x-1, y) else this
+    def right = if (x < 2) Point(x+1, y) else this
+    def up = if (y > 0) Point(x, y-1) else this
+    def down = if (y < 2) Point(x, y+1) else this
+  }
 
-        case ('R', n) if pos.facing == 'N' => Position(pos.x + n, pos.y, 'E')
-        case ('R', n) if pos.facing == 'W' => Position(pos.x, pos.y + n, 'N')
-        case ('R', n) if pos.facing == 'E' => Position(pos.x, pos.y - n, 'S')
-        case ('R', n) if pos.facing == 'S' => Position(pos.x - n, pos.y, 'W')
+//  def computeLine(line: Seq[Char])
+
+  val res = Inputs.day2.lines.toList.scanLeft(Point(1,1)) { (fst, snd) =>
+    snd.foldLeft(fst) { (prev, curr) =>
+      curr match {
+        case 'L' => prev.left
+        case 'R' => prev.right
+        case 'U' => prev.up
+        case 'D' => prev.down
       }
     }
-  //    .toList
-  //    .map(pos => pos.x -> pos.y)
+  }
 
-  val dest = positions.diff(positions.distinct).head
-  //  val dest = positions.filter(revisited.contains)
-
-  println(positions)
-  println(dest)
-  //  println(Math.abs(dest.x) + Math.abs(dest.y))
+  println(res.tail)
 
 
 
